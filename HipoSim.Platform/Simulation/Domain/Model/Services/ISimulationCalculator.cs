0a53ca8bcc482361cf3270ce5de05dd997f00dc5
GeGuMaGu25 <gustavoolivares302@gmail.com>
2026-09-13 11:51:24 -0500

    feat(simulation): implement domain service for french amortization calculation

---
+++ HipoSim.Platform/Simulation/Domain/Model/Services/ISimulationCalculator.cs
@@ -0,0 +1,9 @@
+﻿using HipoSim.Platform.Simulation.Domain.Model.Entities;
+using HipoSim.Platform.Simulation.Domain.Model.ValueObjects;
+
+namespace HipoSim.Platform.Simulation.Domain.Model.Services;
+
+public interface ISimulationCalculator
+{
+    Money CalculateMonthlyPayment(CreditSimulation simulation);
+}
