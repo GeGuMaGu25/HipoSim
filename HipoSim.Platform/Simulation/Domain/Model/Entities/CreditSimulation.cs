61cf28bdd514c625b8050c0b6aee881420919ed0
GeGuMaGu25 <gustavoolivares302@gmail.com>
2026-09-13 11:36:41 -0500

    feat(simulation): add CreditSimulation entity with core business validations

---
+++ HipoSim.Platform/Simulation/Domain/Model/Entities/CreditSimulation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using HipoSim.Platform.Simulation.Domain.Model.ValueObjects;
+
+namespace HipoSim.Platform.Simulation.Domain.Model.Entities;
+
+public class CreditSimulation
+{
+    public Guid Id { get; private set; }
+    public Money PropertyValue { get; private set; }
+    public Money DownPayment { get; private set; }
+    public InterestRate AnnualInterestRate { get; private set; }
+    public int TermInYears { get; private set; }
+
+    public CreditSimulation(Money propertyValue, Money downPayment, InterestRate annualInterestRate, int termInYears)
+    {
+        if (termInYears <= 0 || termInYears > 30)
+        {
+            throw new ArgumentException("El plazo debe ser mayor a 0 y hasta un máximo de 30 años.", nameof(termInYears));
+        }
+
+        if (downPayment.Amount >= propertyValue.Amount)
+        {
+            throw new ArgumentException("La cuota inicial no puede ser mayor o igual al valor de inmueble.");
+        }
+
+        if (propertyValue.Currency != downPayment.Currency)
+        {
+            throw new ArgumentException("La moneda del inmueble y la cuota inicial deben coincidir.");
+        }
+        
+        Id = Guid.NewGuid();
+        DownPayment = downPayment;
+        AnnualInterestRate = annualInterestRate;
+        TermInYears = termInYears;
+        PropertyValue = propertyValue;
+    }
+
+    public Money CalculateLoanAmount()
+    {
+        return new Money(PropertyValue.Amount - DownPayment.Amount, PropertyValue.Currency);
+    }
+}
